c065ebd5d010d625ae252a2c9fb9fc0208eb4c2f
chino <alexhdzv00@gmail.com>
2022-09-07 22:13:22 -0500

    mysite

--- prueba2/models.py
+++ prueba2/models.py
@@ -1,17 +1,21 @@
 from django.db import models
+#from django.db import settings
+#from django.db import timezone
 
 # Create your models here.
 
 class endNum(models.Model):
 
-    net = models.IntegerField(blank=True, null=True)
+    #net = models.IntegerField(blank=True, null=True)
 
-    net2 = models.IntegerField(blank=True, null=True)
+    #net2 = models.IntegerField(blank=True, null=True)
 
-    net3 = models.IntegerField(blank=True, null=True)
+    #net3 = models.IntegerField(blank=True, null=True)
 
-    flop = models.FloatField(blank=True, null=True)
+    #flop = models.FloatField(blank=True, null=True)
+    x1  = models.FloatField(blank=True, null=True)
+    x2  = models.FloatField(blank=True, null=True)
+    x3  = models.FloatField(blank=True, null=True)
 
     def __str__(self):
-
-        return str(self.net)
+        return str(self.x1, self.x2, self.x3)
--- prueba3/apps.py
+++ prueba3/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
-
+#prueba de cambios
 class Prueba3Config(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'prueba3'

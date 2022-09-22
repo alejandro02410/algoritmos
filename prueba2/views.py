cebb44abe0faed8144ecc8a154adb8d5cbb450a9
alejandro02410 <110651160+alejandro02410@users.noreply.github.com>
2022-09-21 19:44:43 -0500

    Update views.py

--- prueba2/views.py
+++ prueba2/views.py
@@ -7,3 +7,10 @@ def endNum(request):
     consulta = endNum.objects.all()
     contexto = {'datos ': consulta}
     return render(request, 'prueba2/index.html',)
+
+def calculaSuma(l):
+    listaSuma=[]
+    for i in l:
+        r = i.x1 + i.x2 + i.x3
+        listaSuma.append(r)
+    return listaSuma
